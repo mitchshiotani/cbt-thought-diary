5b49ad842b84f5ff0c1db103ac93f8e1b7d3b0e0
Mitch Shiotani <mitch.shiotani1@gmail.com>
2019-09-01 21:39:34 +0900

    added simple navbar and multistep form

--- app/models/diary_entry.rb
+++ app/models/diary_entry.rb
@@ -3,4 +3,37 @@ class DiaryEntry < ApplicationRecord
   has_many :emotions, through: :de_emotions
   has_many :deut_styles
   has_many :ut_styles, through: :deut_styles
+
+  attr_writer :current_step
+
+  def current_step
+    @current_step || steps.first
+  end
+
+  def steps
+    %w[a c b d e]
+  end
+
+  def next_step
+    self.current_step = steps[steps.index(current_step) + 1]
+  end
+
+  def previous_step
+    self.current_step = steps[steps.index(current_step) - 1]
+  end
+
+  def first_step?
+    current_step == steps.first
+  end
+
+  def last_step?
+    current_step == steps.last
+  end
+
+  def all_valid?
+    steps.all? do |step|
+      self.current_step = step
+      valid?
+    end
+  end
 end
